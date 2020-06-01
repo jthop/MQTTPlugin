bfe8653161dcbee7159d3c8a961faa3a0cf3aeac
Jamie Hopper <jamie@mhztech.com>
2020-06-01 14:52:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup
 setup(name='MQTTPlugin',
   version='0.1',
   py_modules=['MQTTPlugin'],
-  install_requires=[
-    'setuptools>=40.5.0',
-    'paho-mqtt>=1.5.0'
-    ],
+  install_requires=['setuptools', 'paho-mqtt']
   entry_points={'pynx584': ['test=src.MQTTPlugin:MQTTBridge']},
 )
